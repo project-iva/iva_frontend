75593aba55e0af962b9ab91e9485da9268503756
Igor Pidik <igorPidik@gmail.com>
2021-06-27 13:29:59 +0200

    async trunk data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import IvaCommunicator from './iva_communicator/ivaCommunicator'
 import CommandHandler from './iva_communicator/commandHandler'
 import RoutineModal from './components/routineModal'
 import { connect, ConnectedProps } from 'react-redux'
-import type { RootState } from './store/store'
+import type { RootState, AppDispatch } from './store/store'
+import { fetchMindfulSessions } from './store/mindfulSessionsSlice'
+import { bindActionCreators } from 'redux'
 
 const mapStateToProps = (state: RootState) => {
   return { mindfulSessions: state.mindfulSessions }
 }
 
-const connector = connect(mapStateToProps)
+const mapDispatchToProps = (dispatch: AppDispatch) => {
+  return bindActionCreators({ fetchMindfulSessions }, dispatch)
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps)
 type PropsFromRedux = ConnectedProps<typeof connector>
 
 interface AppProps extends PropsFromRedux {}
@@ -28,6 +34,11 @@ export class App extends Component<AppProps, AppState> {
   componentDidMount() {
     let s = this.props.mindfulSessions
     console.log(s)
+    this.props.fetchMindfulSessions()
+    setTimeout(() => {
+      console.log(this.props.mindfulSessions)
+    }, 1000)
+
     const commandHandler = new CommandHandler(this)
     const communicator = new IvaCommunicator(
       'ws://iva.docker.localhost:5678/web',
--- src/store/mindfulSessionsSlice.ts
+++ src/store/mindfulSessionsSlice.ts
@@ -1,22 +1,60 @@
-import { createSlice } from '@reduxjs/toolkit'
+import {
+  createAsyncThunk,
+  createSlice,
+  SerializedError,
+} from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = [
-  {
-    id: '1',
-    title: 'First Post!',
+interface MindfulSession {
+  uuid: string
+  source_name: string
+  start: string
+  end: string
+}
+
+type MindfulSessionsData = MindfulSession[]
+
+interface MindfulSessionSliceState {
+  mindfulSessions: MindfulSessionsData
+  status: 'idle' | 'pending' | 'succeeded' | 'failed'
+  error: SerializedError | null
+}
+
+const initialState: MindfulSessionSliceState = {
+  mindfulSessions: [],
+  status: 'idle',
+  error: null,
+}
+
+export const fetchMindfulSessions = createAsyncThunk(
+  'mindfulSessions/fetchMindfulSessions',
+  async () => {
+    const response = await axios
+      .get('http://iva-backend.docker.localhost/api/mindful-sessions/')
+      .then((res) => res)
+    return response.data as MindfulSessionsData
   },
-  {
-    id: '2',
-    title: 'Second Post',
-  },
-]
+)
 
 const mindfulSessionsSlice = createSlice({
   name: 'mindfulSessions',
   initialState,
   reducers: {},
-})
+  extraReducers: (builder) => {
+    builder.addCase(fetchMindfulSessions.pending, (state, action) => {
+      state.status = 'pending'
+    })
 
-// export const { } = postsSlice.actions
+    builder.addCase(fetchMindfulSessions.fulfilled, (state, action) => {
+      state.status = 'succeeded'
+      state.mindfulSessions = action.payload
+    })
+
+    builder.addCase(fetchMindfulSessions.rejected, (state, action) => {
+      state.status = 'failed'
+      state.error = action.error
+    })
+  },
+})
 
 export default mindfulSessionsSlice.reducer
